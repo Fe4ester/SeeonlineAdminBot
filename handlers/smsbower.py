5ecc0e4a4b4076c99ee28781ac89c8e3dbf27a38
Fe4ester <xdxdxd4305@gmail.com>
2025-02-14 01:05:37 +0200

    added new api methods

--- handlers/smsbower.py
+++ handlers/smsbower.py
@@ -1,36 +1,60 @@
 from aiogram import Router
 from aiogram.types import Message
-
 from aiogram.filters import Command
-
 from services.smsbower_api import SmsBowerAPI
+
+from config import load_config
+from services.countries import countries
 
 router = Router()
 
+config = load_config()
+
+
 @router.message(Command('smsbower'))
 async def cmd_smsbower(message: Message):
-    pass
+    # Баланс
+    api = SmsBowerAPI(config.SMSBOWER_API_URL)
+    balance = await api.get_balance(config.SMSBOWER_TOKEN)
 
-@router.message(Command('api_test'))
-async def cmd_api_test(message: Message):
-    """
-    Показываем, как вызывать внешний API из бота.
-    Допустим, вы хотели получить какие-то данные, например /todos/1.
-    """
-    # Создаём экземпляр класса ExternalAPI прямо тут или получаем откуда-то DI
-    api = SmsBowerAPI("https://jsonplaceholder.typicode.com")
+    await message.answer(f"""
+Текущий баланс: {balance}
+Удачи поторговаться в этой ебаной колымаге
 
-    data = await api.get_data("/todos/1")
-    # data будет словарём: {"userId":..., "id":..., "title":..., "completed":...}
+Работа чисто с командами - /smsbower_commands
+""")
 
-    # Формируем текст ответа
-    user_id = data.get("userId")
-    title = data.get("title")
-    completed = data.get("completed")
 
-    text = (f"Данные из внешнего API:\n"
-            f"UserID: {user_id}\n"
-            f"Title: {title}\n"
-            f"Completed: {completed}")
+@router.message(Command('get_prices'))
+async def cmd_get_prices(message: Message):
+    api = SmsBowerAPI(config.SMSBOWER_API_URL)
 
-    await message.answer(text)
+    data = await api.get_prices(config.SMSBOWER_TOKEN)
+
+    result = []
+    for country_id, details in data.items():
+        country_name = countries.get(int(country_id), "Unknown Country")
+        cost = details["tg"]["cost"]
+        count = details["tg"]["count"]
+        result.append(f"({country_id}) {country_name}: {cost} - {count}")
+
+    prices = "\n".join(result)
+
+    await message.answer(prices)
+
+
+@router.message(Command('get_balance'))
+async def cmd_get_balance(message: Message):
+    api = SmsBowerAPI(config.SMSBOWER_API_URL)
+
+    balance = await api.get_balance(config.SMSBOWER_TOKEN)
+
+    await message.answer(f"Баланс епта: {balance}")
+
+
+@router.message(Command('smsbower_commands'))
+async def cmd_smsbower_commands(message: Message):
+    await message.answer("""
+/get_balance - Баланс
+/get_prices - Лист номеров и цен
+""")
--- services/smsbower_api.py
+++ services/smsbower_api.py
@@ -1,15 +1,44 @@
 import aiohttp
+import re
+import json
+
 
 class SmsBowerAPI:
     def __init__(self, base_url: str):
         self.base_url = base_url
 
-    async def get_data(self, endpoint: str) -> dict:
-        """
-        Пример базового GET-запроса к внешнему API.
-        endpoint: например, "/todos/1"
-        """
-        url = f"{self.base_url}{endpoint}"
+    async def get_balance(self, token: str):
+        url = f"{self.base_url}?api_key={token}&action=getBalance"
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
-                return await response.json()
+                data = await response.text()
+
+                match = re.search(r"ACCESS_BALANCE:(\d+(\.\d+)?)", data)
+                if match:
+                    balance = float(match.group(1))
+                else:
+                    balance = 0.0
+
+                return balance
+
+    async def get_prices(
+            self, token: str, country: int = None, version: int = 1, max_price: float = 40, min_count: int = 5
+    ):
+        if version == 1:
+            if country:
+                url = f"{self.base_url}?api_key={token}&action=getPrices&service=tg&country={country}"
+            else:
+                url = f"{self.base_url}?api_key={token}&action=getPrices&service=tg"
+
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    text_data = await response.text()
+                    data = json.loads(text_data)
+
+                    filtered_data = {
+                        key: value for key, value in data.items()
+                        if value.get("tg", {}).get("cost", float("inf")) <= max_price
+                           and value.get("tg", {}).get("count", 0) >= min_count
+                    }
+
+                    return filtered_data
