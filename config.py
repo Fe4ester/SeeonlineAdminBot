c9db89fd08247d31ff8e7f4071527709f9e7abea
Fe4ester <xdxdxd4305@gmail.com>
2025-02-20 14:57:30 +0200

    добавил редис в зависимости

--- config.py
+++ config.py
@@ -2,15 +2,23 @@ import os
 from dataclasses import dataclass
 from dotenv import load_dotenv
 
+from aiogram.fsm.storage.redis import RedisStorage, DefaultKeyBuilder
+
+from redis.asyncio import Redis
+
 load_dotenv()
 
 
 @dataclass
 class Config:
     BOT_TOKEN: str
+
     SMSBOWER_API_URL: str
     SMSBOWER_TOKEN: str
+
     SEEONLINE_API_URL: str
+
+    REDIS_URL: str
 
 
 def load_config() -> Config:
@@ -18,5 +26,12 @@ def load_config() -> Config:
         BOT_TOKEN=os.getenv("BOT_TOKEN"),
         SMSBOWER_API_URL=os.getenv("SMSBOWER_API_URL"),
         SMSBOWER_TOKEN=os.getenv("SMSBOWER_TOKEN"),
-        SEEONLINE_API_URL=os.getenv("SEEONLINE_API_URL")
+        SEEONLINE_API_URL=os.getenv("SEEONLINE_API_URL"),
+        REDIS_URL=os.getenv("REDIS_URL")
     )
+
+
+redis_states = Redis.from_url(f"{os.getenv("REDIS_URL")}/0")
+redis_cache = Redis.from_url(f"{os.getenv("REDIS_URL")}/1")
+
+storage = RedisStorage(redis=redis_states, key_builder=DefaultKeyBuilder(with_bot_id=True))
